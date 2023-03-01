b83f13657d134c5d020fa9ebb0fd95e14ab2d94c
highjune <highjune37@gmail.com>
2023-03-01 23:59:39 +0900

    순수jdbc 조회 완성

--- src/test/java/hello/springbootall/basic/kyh_lecture_db1/repository/MemberRepositoryTest.java
+++ src/test/java/hello/springbootall/basic/kyh_lecture_db1/repository/MemberRepositoryTest.java
@@ -2,17 +2,28 @@ package hello.springbootall.basic.kyh_lecture_db1.repository;
 
 import hello.springbootall.kyh_lecture_db1.domain.Member;
 import hello.springbootall.kyh_lecture_db1.repository.MemberRepositoryVO;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+@Slf4j
 class MemberRepositoryTest {
 
-    MemberRepositoryVO memberRepositoryVO = new MemberRepositoryVO();
+    MemberRepositoryVO repository = new MemberRepositoryVO();
 
     @Test
-    void save() throws SQLException {
-        Member member = new Member("memberV1", 10000);
-        memberRepositoryVO.save(member);
+    void crud() throws SQLException {
+        //save
+        Member member = new Member("memberV4", 10000);
+        repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        log.info("membrer == findMember {}", member == findMember);
+        log.info("membrer equals findMember {}", member.equals(findMember));
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
